557a6d8b3605e2f8caf11ce9c1ddf160d2e05a0a
Princess-Morris <okpataprincessmorris3@gmail.com>
2023-11-30 16:10:42 +0100

    created a page to add task

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,34 @@
+import { Box, Checkbox, FormControl, FormHelperText, FormLabel, Input, Textarea } from "@chakra-ui/react";
+import { Form } from "react-router-dom";
+
 export default function Create () {
     return (
-      <div>
-        create
-      </div>
+      <Box maxW='480px'>
+         <Form>
+          <FormControl isRequired mb='40px'>
+            <FormLabel>Task name:</FormLabel>
+            <Input type="text" name='title' />
+            <FormHelperText>Enter a descriptive task name.</FormHelperText>
+          </FormControl>
+
+          <FormControl mb='40px'>
+            <FormLabel>Task description:</FormLabel>
+            <Textarea
+              placeholder="Enter a detailed description for the task..." 
+              name='description'
+              />
+          </FormControl>
+
+          <FormControl display='flex' alignItems='center' mb='4opx'>
+            <Checkbox
+            name="isPriority"
+             size='lg'
+             colorScheme="purple"
+              />
+              <FormLabel mb='0' ml='10px'>Make this a priority task</FormLabel>
+          </FormControl>
+         </Form>
+      </Box>
     );
   }
   
